384c27989a78b311f797e749c43c89b9b0994612
Takayuki Muranushi <muranushi@gmail.com>
2013-02-11 15:33:30 +0900

    remove duplicate orphan instances.

--- Test/Tensor.hs
+++ Test/Tensor.hs
@@ -23,21 +23,14 @@ testTensor = testGroup "test for tensor" $ tests
       makeTest "add zero test" (\x -> x + zero44 == x) ,
       makeTest "subtraction test" (\x -> zero44 - x == negate x) ,
       makeTest "negate test" (\x -> x + negate x == zero44) ,
-      makeTest "multiply zero test" 
+      makeTest "multiply zero test"
         (\x -> compose (\i -> contract (\j -> compose (\k -> x!i!j * zero44!j!k))) == zero44) ,
       makeTest "commutativity test" (\x y -> x + y == y + x && x - x == zero44) ,
       makeTest "associativity test" (\x y z-> x + (y+z) == (x+y) + z && x - x == zero44) ,
-      makeTest "multiply notation test" 
-        (\x y -> compose (\i -> contract (\j -> compose (\k -> x!i!j * y!j!k))) - 
-                 compose (\a -> contract (\b -> compose (\c -> y!b!c * x!a!b))) + y + x 
-                 == zero44 + x + y) 
+      makeTest "multiply notation test"
+        (\x y -> compose (\i -> contract (\j -> compose (\k -> x!i!j * y!j!k))) -
+                 compose (\a -> contract (\b -> compose (\c -> y!b!c * x!a!b))) + y + x
+                 == zero44 + x + y)
       ]
-    makeTest name test = 
-      testResult name $ quickCheckWithResult stdArgs{maxSize=1000, chatty=False} $ test 
-
-
-instance Arbitrary a => Arbitrary (Vec a) where
-  arbitrary = elements [Vec]
-
-instance (Arbitrary a, Arbitrary (n a)) => Arbitrary (n :~ a) where
-  arbitrary = fmap (\(v, x) -> v :~ x) arbitrary
+    makeTest name test =
+      testResult name $ quickCheckWithResult stdArgs{maxSize=1000, chatty=False} $ test
